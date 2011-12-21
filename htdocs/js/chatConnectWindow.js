d3e56230e9e737ff41e18b731198dfafdff9534b
Tom Powell <zifnab@zifnab06.net>
2011-12-21 16:39:23 -0700

    sasl v2

--- htdocs/js/chatConnectWindow.js
+++ htdocs/js/chatConnectWindow.js
@@ -12,6 +12,7 @@ Object.extend(Object.extend(chatConnectWindow.prototype, chatWindow.prototype), 
 		$(this.divContent).update('<div class="list_content">'+
 		                               'Enter your nickname:<br />'+
 		                               '<div class="nickname_input" id="'+this.divNickname+'"><input type="text" name="input_nickname" id="input_nickname" /></div><br />'+
+					       'Auth to nickserv?<br />'+
                                                'Optionally, enter your NickServ password:<br />'+
 		                               '<div class="password_input" id="'+this.divPassword+'"><input type="password" name="input_password" id="input_password" /></div><br />'+
 		                               '<div class="button" id="connect_button"><div class="button_left"></div><div class="button_center"><div class="button_text">Connect</div></div><div class="button_right"></div></div>'+
@@ -30,9 +31,6 @@ Object.extend(Object.extend(chatConnectWindow.prototype, chatWindow.prototype), 
 		server = $A(chat.connectWindow.localServers).random();
 		
 		if (server && nickname != '' && nickname != undefined) {
-			chat.connect(nickname, server);
-		}
-		if (server && nickname && password != '' && nickname != undefined && password != undefined){
 			chat.connect(nickname, password, server);
 		}
 	}
