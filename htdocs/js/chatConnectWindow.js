1b0a18b4e40ab228442edce882c95144ffe8c0cb
Tom Powell <zifnab@zifnab06.net>
2011-12-24 16:54:39 -0700

    fixed stupid error

--- htdocs/js/chatConnectWindow.js
+++ htdocs/js/chatConnectWindow.js
@@ -6,14 +6,10 @@ Object.extend(Object.extend(chatConnectWindow.prototype, chatWindow.prototype), 
 	initialize: function(id) {
 		this.windowInitialize(id, arguments[1] || {});
 		this.divNickname = this.id+'_nickname_input';
-		this.divPassword = this.id+'_password_input';
-		this.divNetwork  = this.id+'_network_select';
 		this.localServers    = ['127.0.0.1'];
 		$(this.divContent).update('<div class="list_content">'+
 		                               'Enter your nickname:<br />'+
 		                               '<div class="nickname_input" id="'+this.divNickname+'"><input type="text" name="input_nickname" id="input_nickname" /></div><br />'+
-//                                               'Optionally, enter your NickServ password:<br />'+
-//		                               '<div class="password_input" id="'+this.divPassword+'"><input type="password" name="input_password" id="input_password" /></div><br />'+
 					       'Current known issues located <a href="https://github.com/zifnab06/webchat/issues" target="_blank">here</a>.'+
 		                               '<div class="button" id="connect_button"><div class="button_left"></div><div class="button_center"><div class="button_text">Connect</div></div><div class="button_right"></div></div>'+
 		                               '</div>');
@@ -27,7 +23,6 @@ Object.extend(Object.extend(chatConnectWindow.prototype, chatWindow.prototype), 
 		}
 		var server   = '';
 		var nickname = $F('input_nickname');
-		var password = $F('input_password');
 		server = $A(chat.connectWindow.localServers).random();
 		
 		if (server && nickname != '' && nickname != undefined) {
