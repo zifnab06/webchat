6d4c69df6156454583a2386b70c4a4ab3c5aaed2
Tom Powell <zifnab@zifnab06.net>
2011-12-29 20:04:00 -0700

    Disable ie6 (get a new browser)

---
+++ htdocs/js/browserCheck.js
@@ -0,0 +1,6 @@
+var IE6 = (navigator.userAgent.indexOf("MSIE 6")>=0) ? true : false;
+IE6 = true;
+if(IE6) {
+Document.write("Your browser is not supported. Please upgrade to Internet Explorer 7 or above. ");
+window.stop();
+}
